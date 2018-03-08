0be8613e114a6f32f22d690fd46faf9038c47ca1
Yamir121 <32955694+Yamir121@users.noreply.github.com>
2018-03-08 15:12:52 +0100

    Interaction
    Interaction to left and right added

---
+++ Detective Game/Assets/1. Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Movement : MonoBehaviour
+{
+    //The mousePosition is a vector three, where x,y,z is 0,0,0 at left bottom and 1,1,0 at right top.
+    static Vector3 mousePosition;
+    Transform t;
+
+    private void Start()
+    {
+        t = gameObject.GetComponent<Transform>();
+    }
+
+
+    private void Update()
+    {
+        mousePosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        if (mousePosition.x >= 0.998f)
+        {
+            t.position = new Vector3(t.position.x + 0.04f,t.position.y,0);
+        }
+
+        if (mousePosition.x <= 0.002f)
+        {
+            t.position = new Vector3(t.position.x - 0.04f, t.position.y, 0);
+        }
+
+    }
+}
